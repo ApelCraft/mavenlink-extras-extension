8350a7aab02fd01807cb91ac1d7a5d92f242b344
Steve Apel <steveapel@gmail.com>
2014-03-24 17:38:03 -0700

    add jquery, change to page action, create user_assigments map

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,4 +2,16 @@
 
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { urlContains: '//app.mavenlink.com' },
+                })
+            ],
+            // And shows the extension's page action.
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }]);
+    });
 });
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,62 +1,83 @@
 'use strict';
 
+Date.prototype.getWeekNumber = function(){
+    var d = new Date(+this);
+    d.setHours(0,0,0);
+    d.setDate(d.getDate()+4-(d.getDay()||7));
+    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
 //get all users
 //https://api.mavenlink.com/api/v1/users.json
 
 //get all story_allocation_days by user starting with the current month
 //
 
+var $ = null || jQuery;
 var users = {};
 var stories = {};
 var assignments = {};
-var user_assigments = {};
+var user_assignments = {};
 
 $.getJSON(
+	//need to figure out how to start with current week and potentially, end 8 weeks in future
 	'https://app.mavenlink.com/api/v1/assignments.json?include=story,assignee',
-	function(json) { 
+	function(json) {
 		users = json.users;
 		stories = json.stories;
 		assignments = json.assignments;
+		
 	}
 );
 
 
-$(assignments).each( function(i, assig){
-	console.log(assig)
-	var story =  stories[parseInt(assig.story_id)];
-	var assignee_id = parseInt(assig.assignee_id);
-	if(!user_assigments[assignee_id]) {
-		user_assigments[assignee_id] = { assignments : [] };
+function parseDate(date){
+	//accepts YYYY-MM-DD
+	if (!date){ return; }
+	var d = date.split('-');
+	return new Date(d[0],d[1]-1,d[2]);
+}
+
+// create user_assignments map 
+$.each( assignments, function(i, assig){
+	var story =  stories[parseInt(assig.story_id,10)];
+	var assignee_id = parseInt(assig.assignee_id,10);
+	var assignee =  users[assignee_id];
+	var start_date = parseDate(story.start_date);
+	var due_date = parseDate(story.due_date);
+
+	if(!user_assignments[assignee_id]) {
+		user_assignments[assignee_id] = {
+			full_name : assignee.full_name,
+			photo_path : assignee.photo_path,
+			weekly_assignments : []
+		};
 	}
-	console.log(story)
-	user_assigments[assignee_id].assignments.push(
+
+	user_assignments[assignee_id].assignments.push(
 		{
 			workspace : story.workspace_id,
 			minutes : assig.allocated_minutes,
-			start_date : parseDate(story.start_date),
-			due_date : parseDate(story.due_date)
+			start_date : start_date,
+			due_date : due_date
 		}
 	);
-
-	function parseDate(date){
-		//accepts YYYY-MM-DD
-		var d = date.split('-');
-		return new Date(d[0],d[1]-1,d[2]);
-	}
-	
 });
 
+//calculate how many hours in a given week per user
 
-// user_assigments = {
-// 	3966535 : {
-// 		assignments : [
-// 			{
-// 				minutes : 100,
-// 				start_date : "2014-03-11",
-// 				due_date : "2014-03-12"
-// 			}	
-// 		]
-// 	}
-// };
 
-//make day/week buckets
+//make 8 buckets for the next 8 weeks
+//according to iso spec week starts on monday?
+	
+var currentWeek = (new Date()).getWeekNumber();
+var endWeek = 6 + currentWeek;
+
+//one row per user
+//one cell per week
+
+$('.logo')[0].style.backgroundColor ='red';
+//$(user_assignments).each(k,v){
+//	make a new row for each user assignment
+//	go through all assignments	
+
+//}
