a4c3ff7e73271ae81e32f8b2b5a97c9bc915b118
Steve Apel <steveapel@gmail.com>
2014-03-27 13:39:14 -0700

    delegate date handling to momentjs

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,24 +1,6 @@
 'use strict';
 
-Date.prototype.getWeekNumber = function(){
-    var d = new Date(+this);
-    d.setHours(0,0,0);
-    d.setDate(d.getDate()+4-(d.getDay()||7));
-    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
-};
-function getDateOfWeek(w, y) {
-    var d = (1 + (w - 1) * 7); // 1st of January + 7 days for each week
-    return new Date(y, 0, d);
-}
-function parseDate(date){
-	//accepts YYYY-MM-DD
-	if (!date){ return; }
-	var d = date.split('-');
-	return new Date(d[0],d[1]-1,d[2]);
-}
-
-var $ = null || jQuery,
-		users = {},
+var users = {},
 		stories = {},
 		assignments = {},
 		user_assignments = {};
@@ -26,11 +8,9 @@ var $ = null || jQuery,
 // create user_assignments map 
 function build_user_assignments(){
 	$.each( assignments, function(i, assig){
-		var story =  stories[parseInt(assig.story_id,10)];
-		var assignee_id = parseInt(assig.assignee_id,10);
-		var assignee =  users[assignee_id];
-		var start_date = parseDate(story.start_date);
-		var due_date = parseDate(story.due_date);
+		var story =  stories[parseInt(assig.story_id,10)],
+				assignee_id = parseInt(assig.assignee_id,10),
+				assignee =  users[assignee_id];
 
 		if(!user_assignments[assignee_id]) {
 			user_assignments[assignee_id] = {
@@ -40,13 +20,14 @@ function build_user_assignments(){
 				week_hours : {}
 			};
 		}
-
+		// fill up the assignments array
 		user_assignments[assignee_id].assignments.push(
 			{
 				workspace : story.workspace_id,
 				hours : assig.allocated_minutes/60,
-				start_date : start_date,
-				due_date : due_date
+				start_date : Date.parse(story.start_date+'T08:00:00'),
+				due_date : Date.parse(story.due_date+'T08:00:00'),
+				title: story.title,
 			}
 		);
 
@@ -56,7 +37,7 @@ function build_user_assignments(){
 function buildUI(){
 	// #TODO start week on saturday instead of monday?
 	// UI shows next 6 weeks
-	var currentWeek = (new Date()).getWeekNumber();
+	var currentWeek = moment().week();
 	var endWeek = 12 + currentWeek; //#todo account for when week wraps around the new year
 	var resultHTML = '';
 
@@ -66,44 +47,55 @@ function buildUI(){
 
 	resultHTML += '<table class="me__resource-table"><tr><th>People</th>';
 	for (var i = currentWeek; i < endWeek+1; i++) {  //#todo account for when week wraps around the new year
-		var d = getDateOfWeek(i, 2014);
-		resultHTML += '<th>WK ' + i + ' ' + (d.getMonth()+1) + '/' + d.getDate() + '</th>';
+		var m = moment().year(2014).week(i); //#TODO need to use a real year hear
+		resultHTML += '<th>WK ' + i + ' ' + m.day(0).format('MM/DD') + '</th>';
 	}
 	resultHTML += '</tr>';
 
+
+
 	$.each( user_assignments, function(i, ua){
-		//sum and populate week_hours
+		var assignmentHTML = '<ul>';
+		//sum and populate week_hours	
 		$.each(ua.assignments, function(i, assig) {
-			if (!assig.start_date || !assig.due_date){ //#find out why these are null sometimes
-				return;
-			}
-			var start_week = assig.start_date ? assig.start_date.getWeekNumber() : (new Date()).getWeekNumber(), //where a task has already been started, the start date will null
-					due_week = assig.due_date.getWeekNumber();
+			//#TODO find out why these are null sometimes
+			if (!assig.start_date || !assig.due_date){ return;}
+
+			var start_week = assig.start_date ? moment(assig.start_date).week() : currentWeek, //where a task has already been started, the start date will null
+					due_week = moment(assig.due_date).week();
 			for (var i = start_week; i < due_week+1; i++) { //#todo account for when week wraps around the new year
-				if(!ua.week_hours[i]) {
-					ua.week_hours[i] = 0;
-				}
-				ua.week_hours[i] = ua.week_hours[i] + (assig.hours / (due_week - start_week + 1));
+				// create if it doesn't exist
+				if(!ua.week_hours[i]) { ua.week_hours[i] = 0; }
+				ua.week_hours[i] += (assig.hours / (due_week - start_week + 1));
 			}
+			// if(assig.due_date > getDateOfWeek(currentWeek, (new Date()).getYear) ){
+			// 	assignmentHTML += '<li>' + assig.workspace + ', ' + assig.title + ', ' + assig.start_date + ' - ' + assig.due_date + '</li>';
+			// }
+
 		});
-		
+		assignmentHTML += '</ul>';
+
 		//one row per user
 		resultHTML += '<tr>';
-		resultHTML += '<td class="'+i+'">' + users[i].full_name + '</td>';
+		resultHTML += '<td data-uid="'+i+'">' + users[i].full_name + '</td>';
 		//one cell per week
-		for (var i = currentWeek; i < endWeek+1; i++) {//#todo account for when week wraps around the new year
+		for (var i = currentWeek; i < endWeek+1; i++) {//#todo account for when week wraps around the new year (if due is less than start)
 			var roundedHours = Math.round((ua.week_hours[i] || 0)*10)/10;
 			resultHTML += '<td><div class="'+ utilizationClass(roundedHours) +'">' + roundedHours + '</td>';
 		}
-		resultHTML += '</tr>';
+		resultHTML += '</tr>' +
+			'<tr><td colspan="13">'+assignmentHTML+'</td></tr>';
 	});
 
 	resultHTML +='</table>';
 	$('body').append(resultHTML);
 }
+
 function fetchData(page){
+	page = page || 1;
 	$.getJSON(
-		//need to figure out how to start with current week and potentially, limit to a # of weeks in future 
+		//need to figure out how to start with current week and potentially, limit to a # of weeks in future. 
+		//Right now it just gets everything except archived projects
 		'https://app.mavenlink.com/api/v1/assignments.json?include=story,assignee&current=true&per_page=200&page='+page,
 		function(json) {
 			$.extend(users, json.users);
@@ -120,4 +112,4 @@ function fetchData(page){
 }
 
 $('.navigation .left-nav-footer').append('<a href="#resource-allocation" id="me--fetch-trigger">Resource Allocation</a>');
-$('#me--fetch-trigger').on('click',function(){ fetchData(1); });
+$('#me--fetch-trigger').on('click',function(){ fetchData(); });
