67e46759d9a6e5a2d00ab62ef3db85312eab80fc
Steve Apel <steveapel@gmail.com>
2014-03-31 14:33:29 -0700

    get assignment rows and split hour duration across cells

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,85 +1,84 @@
 'use strict';
 
-var users = {},
-		stories = {},
-		workspaces = {},
-		assignments = {},
+var Users = {},
+		Stories = {},
+		Workspaces = {},
+		Assignments = {},
 		user_assignments = {},
 		currentMoment = moment();
 
 // create user_assignments map 
 function build_user_assignments(){
-	$.each( assignments, function(i, assig){
-		var story =  stories[parseInt(assig.story_id,10)],
+	$.each( Assignments, function(assig_key, assig){
+		var story_id = parseInt(assig.story_id,10),
+				story =  Stories[story_id],
 				assignee_id = parseInt(assig.assignee_id,10),
-				assignee =  users[assignee_id];
+				assignee =  Users[assignee_id],
+				workspace_id = parseInt(story.workspace_id,10),
+				assig_hours = assig.allocated_minutes/60;
+
+		// bail if the assignment is complete or from a previous week
+		if ( !story.due_date || !moment(story.due_date).isValid() || moment(story.due_date).day(0).isBefore(currentMoment) ){ return;}
 
 		if(!user_assignments[assignee_id]) {
 			user_assignments[assignee_id] = {
 				full_name : assignee.full_name,
 				photo_path : assignee.photo_path,
-				assignments : [],
-				weekly : {},
-				workspace_weekly_assignments : {}
+				assignments : {},
+				workspaces : {},
+				weekly_hours : {}
 			};
 		}
-		// fill up the assignments array
-		user_assignments[assignee_id].assignments.push(
-			{
-				workspace : story.workspace_id,
+		var tmp_assig = {};
+		tmp_assig[story_id] = {
+				workspace_id : workspace_id,
 				hours : assig.allocated_minutes/60,
 				start_date : story.start_date ? moment(story.start_date).toDate() : null, //moment parse is needed to prevent date format YYYY-MM-DD from being parsed as UTC
 				due_date : story.due_date ? moment(story.due_date).toDate() : null,
-				title: story.title,
-				id: parseInt(story.id)
+				title: story.title
+			};
+		// fill up the assignments map
+		$.extend(user_assignments[assignee_id].assignments, tmp_assig);
+
+		// fill up the workspaces map
+		if(!user_assignments[assignee_id].workspaces[workspace_id]){
+			user_assignments[assignee_id].workspaces[workspace_id] = {
+				title  : Workspaces[workspace_id].title,
+				assignments : {}
+			};
+		}
+		
+		// fill up the workspaces.assigments map
+		if( !user_assignments[assignee_id].workspaces[workspace_id].assignments[story_id] ){
+			user_assignments[assignee_id].workspaces[workspace_id].assignments[story_id] = {
+				hours : assig_hours,
+				weekly_hours : {},
+				start_date : story.start_date ? moment(story.start_date).toDate() : null, //moment parse is needed to prevent date format YYYY-MM-DD from being parsed as UTC
+				due_date : story.due_date ? moment(story.due_date).toDate() : null,
+				title: story.title
+			};
+		}
+		
+		var start = story.start_date ? moment(story.start_date) : currentMoment, //where a task has already been started, the start date will be null
+				due = moment(story.due_date),
+				weeksCount = due.day(0).diff(start.day(0), 'weeks')+1;
+
+		//loop through week duration of assignment
+		for (var i = 0; i < weeksCount; i++) {
+			var m = start.clone().day(0).add(i,'weeks');
+			// create weekly key if it doesn't exist
+			var weekly_hours = user_assignments[assignee_id].weekly_hours[m.week()];
+			if(!weekly_hours) {
+				weekly_hours = user_assignments[assignee_id].weekly_hours[m.week()] = {
+					hours: 0
+				};
 			}
-		);
-	});
-	//#TODO shoultdn't have to go through this array again...
-	$.each( user_assignments, function(ua_key, ua){
-		//sum and populate weekly map	
-		$.each(ua.assignments, function(assig_key, assig) {
-			//#TODO find out why due date is null sometimes
-			if ( !assig.due_date || moment(assig.due_date).isBefore(currentMoment) ){ return;}
-			var start = assig.start_date ? moment(assig.start_date) : currentMoment, //where a task has already been started, the start date will be null
-					due = moment(assig.due_date),
-					weeksCount = start.diff(due, 'weeks')+1;
+			// sum hours (split by duration if needed)
+			weekly_hours.hours += (assig_hours / weeksCount);
+			// attach split duration to assigment
+			user_assignments[assignee_id].workspaces[workspace_id].assignments[story_id].weekly_hours[m.week()] = (assig_hours / weeksCount);
+		}
 
-			// create workspace_weekly_hours key if it doesn't exist
-			var wwh = ua.workspace_weekly_assignments[assig.workspace];
-			if(!wwh) {
-				wwh = ua.workspace_weekly_assignments[assig.workspace] = {};
-			}
-
-			//loop through week duration of assignment
-			for (var i = 0; i < weeksCount; i++) {
-				var m = start.clone().add(i,'weeks');
-				// create weekly key if it doesn't exist
-				var weekly = ua.weekly[m.week()];
-				if(!weekly) {
-					weekly = ua.weekly[m.week()] = {
-						hours: 0
-					};
-				}
-				// sum hours (split by duration if needed)
-				weekly.hours += (assig.hours / weeksCount);
-
-				if(!wwh[m.week()]){
-					wwh[m.week()] = {
-						hours : 0,
-						assignments : {}
-					};
-				}
-				wwh[m.week()].hours += (assig.hours / weeksCount);
-
-				if(!wwh[m.week()].assignments[assig_key]){
-					wwh[m.week()].assignments[assig.id] = {
-						hours : assig.hours
-					};
-				}
-
-			}
-		});
 	});
 }
 
@@ -94,17 +93,9 @@ function buildView() {
 	resultHTML += '<table class="me__resource-table"><tr><th>People</th>';
 	for (var i = 0; i < futureWeekCount; i++) {
 		var m = currentMoment.clone().add(i,'weeks');
-		resultHTML += '<th>WK ' + m.week() + ' ' + m.day(0).format('MM/DD') + '</th>';
+		resultHTML += '<th>WK-' + m.week() + ' ' + m.day(0).format('MM/DD') + '</th>';
 	}
 	resultHTML += '</tr>';
-
-	function assignmentList(assigs){
-		var html = '';
-		$.each(assigs, function(k,v){
-			html += '<br/>'+ stories[k].title;
-		});
-		return html;
-	}
 
 	$.each( user_assignments, function(ua_key, ua){
 		//one row per user
@@ -112,41 +103,47 @@ function buildView() {
 		var hourSumCells = '';
 		for (var i = 0; i < futureWeekCount; i++) {
 			var m = currentMoment.clone().add(i,'weeks');
-			var roundedHours = ua.weekly[ m.week() ] ? Math.round((ua.weekly[ m.week() ].hours || 0)*10)/10 : 0;
+			var roundedHours = ua.weekly_hours[ m.week() ] ? Math.round((ua.weekly_hours[ m.week() ].hours || 0)*10)/10 : 0;
 			hourSumCells += '<td><div class="'+ utilizationClass(roundedHours) +'">' + roundedHours + '</td>';
-			//var workspaceHourCells += '<td><div class="'+ utilizationClass(roundedHours) +'">' + roundedHours + '</td>';
 		}
 
-		//one row per project
-		//one cell per week per project
-		//one row per project assigment
-		//assignmentHTML += '<li>' + assig.workspace + ', ' + assig.title + ', ' + start.format('MM/DD') + ' - ' + due.format('MM/DD') + '</li>';
-		var projectRows = '';
-		$.each( ua.workspace_weekly_assignments, function(wwa_key, wwa){
+		
+		//one row per workspace
+		var workspaceRows = '';
+		$.each( ua.workspaces, function(workspace_key, workspace){
 			
-			var projectCells = '';
+			//one row per assigment
+			var assignmentRows = '';
+			$.each( workspace.assignments, function(assig_key, assig){
+				var assignmentHoursCells = '';
+				//one cell per week
+				for (var i = 0; i < futureWeekCount; i++) {
+					var m = currentMoment.clone().day(0).add(i,'weeks');
+					var roundedHours = assig.weekly_hours[m.week()] ? Math.round(assig.weekly_hours[ m.week() ]*10)/10 : 0;
+					assignmentHoursCells += '<td>' + roundedHours + '</td>';
+				}
 
-			// #TODO consolidate this week loop with the previous one
-			for (var i = 0; i < futureWeekCount; i++) {
-				var m = currentMoment.clone().add(i,'weeks');
-				var roundedHours = wwa[ m.week() ] ? Math.round((wwa[ m.week() ].hours || 0)*10)/10 : 0;
-				projectCells += '<td>' + roundedHours + (wwa[ m.week() ] ? assignmentList(wwa[ m.week() ].assignments) : '') + '</td>';
-			}
+				assignmentRows +=
+					'<tr class="assignment-row">' +
+						'<td data-assignmentid="'+assig_key+'">' + assig.title + ' - ' + assig.hours + '</td>' +
+						assignmentHoursCells +
+					'</tr>';
+			});
 
-			projectRows +=
-				'<tr>' +
-					'<td data-wid="'+wwa_key+'">' + workspaces[wwa_key].title + '</td>' +
-					projectCells +
-				'</tr>';
+			workspaceRows +=
+				'<tr class="workspace-row">' +
+					'<td data-workspaceid="'+workspace_key+'" colspan="'+futureWeekCount+'">' + Workspaces[workspace_key].title + '</td>' +
+				'</tr>' +
+				assignmentRows;
 
 		});
 
 		resultHTML +=
-			'<tr>' +
-				'<td data-uid="'+i+'">' + users[ua_key].full_name + '</td>' +
+			'<tr  class="user-row">' +
+				'<td data-uid="'+i+'">' + Users[ua_key].full_name + '</td>' +
 				hourSumCells +
 			'</tr>'+
-			projectRows;
+			workspaceRows;
 	});
 
 	resultHTML +='</table>';
@@ -160,7 +157,7 @@ function fetchData(){
 		$.getJSON(
 			'https://app.mavenlink.com/api/v1/workspaces.json?&per_page=200&page='+page,
 			function(json) {
-				$.extend(workspaces, json.workspaces);
+				$.extend(Workspaces, json.workspaces);
 				if(json.count && json.count > page * 200){
 					fetchWorkspaces(page+1);
 				} else {
@@ -179,9 +176,9 @@ function fetchData(){
 			//Right now it just gets everything except archived projects
 			'https://app.mavenlink.com/api/v1/assignments.json?include=story,assignee&current=true&per_page=200&page='+page,
 			function(json) {
-				$.extend(users, json.users);
-				$.extend(stories, json.stories);
-				$.extend(assignments, json.assignments);
+				$.extend(Users, json.users);
+				$.extend(Stories, json.stories);
+				$.extend(Assignments, json.assignments);
 				if(json.count && json.count > page * 200){
 					fetchAssignments(page+1);
 				} else {
